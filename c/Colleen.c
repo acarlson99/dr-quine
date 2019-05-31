2cf5f5fe065847922b62bff0e803004c6c20ae4a
Alexander <alexandercarlson99@gmail.com>
2019-05-31 16:17:07 -0700

    Fixed comments

--- c/Colleen.c
+++ c/Colleen.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /*
-** One comment
+  One comment
 */
 
 /*
-** Two comment
+  Two comment
 */
 
 char	*other(void) {
-	return ("#include <stdio.h>%c%c/*%c** One comment%c*/%c%c/*%c** Two comment%c*/%c%cchar	*other(void) {%c%creturn (%c%s%c);%c}%c%cint		main(void) {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
+	return ("#include <stdio.h>%c%c/*%c  One comment%c*/%c%c/*%c  Two comment%c*/%c%cchar	*other(void) {%c%creturn (%c%s%c);%c}%c%cint		main(void) {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
 }
 
 int		main(void) {
