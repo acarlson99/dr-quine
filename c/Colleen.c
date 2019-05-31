51082a50c20d5fd6bf440454e94307a5806a4774
Alexander <alexandercarlson99@gmail.com>
2019-05-31 16:01:04 -0700

    Grace

--- c/Colleen.c
+++ c/Colleen.c
@@ -8,10 +8,10 @@
 ** Two comment
 */
 
-char	*other() {
-	return ("#include <stdio.h>%c%c/*%c** One comment%c*/%c%c/*%c** Two comment%c*/%c%cchar	*other() {%c%creturn (%c%s%c);%c}%c%cint		main() {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
+char	*other(void) {
+	return ("#include <stdio.h>%c%c/*%c** One comment%c*/%c%c/*%c** Two comment%c*/%c%cchar	*other(void) {%c%creturn (%c%s%c);%c}%c%cint		main(void) {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
 }
 
-int		main() {
+int		main(void) {
 	printf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);
 }
--- c/Grace.c
+++ c/Grace.c
@@ -1,7 +1,8 @@
-#define A
-#define B
-#define C() int main() {}
-
+#include <stdio.h>
+#include <fcntl.h>
+#define A "#include <stdio.h>%c#include <fcntl.h>%c#define A %c%s%c%c#define B %c%s%c%c#define C() int main() {int fd = open(%c%s%c, O_CREAT|O_WRONLY, S_IRWXU); if (fd < 0) return (1); dprintf(fd, A, 10, 10, 34, A, 34, 10, 34, B, 34, 10, 34, B, 34, 10, 10, 10, 10);}%c// 3 defines only, 1 comment, write to Grace_kid.c%c%cC()%c"
+#define B "Grace_kid.c"
+#define C() int main() {int fd = open("Grace_kid.c", O_CREAT|O_WRONLY, S_IRWXU); if (fd < 0) return (1); dprintf(fd, A, 10, 10, 34, A, 34, 10, 34, B, 34, 10, 34, B, 34, 10, 10, 10, 10);}
 // 3 defines only, 1 comment, write to Grace_kid.c
 
 C()
