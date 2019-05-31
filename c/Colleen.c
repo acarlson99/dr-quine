126b2c849695884ca00e7b6c669c7fc9be9f8b99
Alexander <alexandercarlson99@gmail.com>
2019-05-31 15:07:34 -0700

    Collen'd it

--- c/Colleen.c
+++ c/Colleen.c
@@ -8,10 +8,10 @@
 ** Two comment
 */
 
-int other() {
-	return (0);
+char	*other() {
+	return ("#include <stdio.h>%c%c/*%c** One comment%c*/%c%c/*%c** Two comment%c*/%c%cchar	*other() {%c%creturn (%c%s%c);%c}%c%cint		main() {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
 }
 
-int main() {
-	printf("Yee\n");
+int		main() {
+	printf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);
 }
