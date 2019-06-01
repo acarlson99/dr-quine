5841c2678a91e532b29dec9f29b99ae9d9ddb417
Alexander <alexandercarlson99@gmail.com>
2019-06-01 16:45:01 -0700

    Done with Colleen

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,10 +1,20 @@
 section .data
-fmt: db "section .data%cfmt: db %c%s%c%csection .text%cglobal start%cglobal _main%cextern _printf%c_main:%c	push rbp%c	mov rbp, rsp%c	lea rdi, [rel fmt]%c	mov rsi, 10%c	mov rdx, 34%c	lea rcx, [rel fmt]%c	mov r8, 34%c	mov r9, 10%c	mov r14, 28%c.loop:%c	push rsi%c	dec r14%c	cmp r14, 0%c	jne .loop%c	call _printf%c	mov rax, 0%c	leave%c	ret%c_printboi:%c	push rbp%c	mov rbp, rsp%c	leave%c	ret%c"
+fmt: db "section .data%cfmt: db %c%s%c%c%csection .text%cglobal start%cglobal _main%cextern _printf%c%c_main:%c	push rbp%c	mov rbp, rsp%c	; print tha boiiiiiiiiiiiiiii%c	call _printboi%c	leave%c	ret%c%c_printboi:%c	push rbp%c	mov rbp, rsp%c	lea rdi, [rel fmt]%c	mov rsi, 10%c	mov rdx, 34%c	lea rcx, [rel fmt]%c	mov r8, 34%c	mov r9, 10%c	mov r14, 38%c.loop:%c	push rsi%c	dec r14%c	cmp r14, 0%c	jne .loop%c	call _printf%c	mov rax, 0%c	leave%c	ret%c%c; löööööööp%c"
+
 section .text
 global start
 global _main
 extern _printf
+
 _main:
+	push rbp
+	mov rbp, rsp
+	; print tha boiiiiiiiiiiiiiii
+	call _printboi
+	leave
+	ret
+
+_printboi:
 	push rbp
 	mov rbp, rsp
 	lea rdi, [rel fmt]
@@ -13,7 +23,7 @@ _main:
 	lea rcx, [rel fmt]
 	mov r8, 34
 	mov r9, 10
-	mov r14, 28
+	mov r14, 38
 .loop:
 	push rsi
 	dec r14
@@ -23,8 +33,5 @@ _main:
 	mov rax, 0
 	leave
 	ret
-_printboi:
-	push rbp
-	mov rbp, rsp
-	leave
-	ret
+
+; löööööööp
