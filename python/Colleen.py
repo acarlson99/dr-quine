188326685034489c13d583c7b3bfb27a505c2932
John <john@Jellybean.localdomain>
2019-06-01 02:03:07 -0700

    Fixed bois

--- python/Colleen.py
+++ python/Colleen.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python3
 # Comment A
 def fun():
-    return "#! /usr/bin/env python3%c# Comment A%cdef fun():%c    return %c%s%c%c# Comment B%cdef main():%c    print(fun() %% (10, 10, 10, 34, fun(), 34, 10, 10, 10, 10, 10, 10))%c%cif __name__ == '__main__':%c    main()"
+    return '#! /usr/bin/env python3%c# Comment A%cdef fun():%c    return %r%c# Comment B%cprint(fun() %% (10, 10, 10, fun(), 10, 10))'
 # Comment B
-def main():
-    print(fun() % (10, 10, 10, 34, fun(), 34, 10, 10, 10, 10, 10, 10))
-
-if __name__ == '__main__':
-    main()
+print(fun() % (10, 10, 10, fun(), 10, 10))
