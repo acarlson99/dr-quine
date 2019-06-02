843fc3f37385bb635d4dd3e783d9fda5b99a2b74
Alexander <alexandercarlson99@gmail.com>
2019-06-02 00:51:48 -0700

    Fixed file IO errors

--- c/Sully.c
+++ c/Sully.c
@@ -9,7 +9,9 @@ int main() {
 	char *f;
 	asprintf(&f, FILENAME, i-1);
 	int fd = open(f, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-	char *s = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#define FILENAME %2$c%4$s%2$c%1$cint main() {%1$c	int i = %3$d; %1$c	if (i <= 0)%1$c		return (1);%1$c	char *f;%1$c	asprintf(&f, FILENAME, i-1);%1$c	int fd = open(f, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);%1$c	char *s = %2$c%5$s%2$c;%1$c	dprintf(fd, s, 10, 34, i-1, FILENAME, s);%1$c	close(fd);%1$c}%1$c";
+	if (fd <= 0)
+		return (1);
+	char *s = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#define FILENAME %2$c%4$s%2$c%1$cint main() {%1$c	int i = %3$d; %1$c	if (i <= 0)%1$c		return (1);%1$c	char *f;%1$c	asprintf(&f, FILENAME, i-1);%1$c	int fd = open(f, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);%1$c	if (fd <= 0)%1$c		return (1);%1$c	char *s = %2$c%5$s%2$c;%1$c	dprintf(fd, s, 10, 34, i-1, FILENAME, s);%1$c	close(fd);%1$c}%1$c";
 	dprintf(fd, s, 10, 34, i-1, FILENAME, s);
 	close(fd);
 }
--- python/Grace.py
+++ python/Grace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 target = 'Grace_kid.py'
-s = '#!/usr/bin/env python3%c%ctarget = %r%cs = %r%c# 🅱️%cwith open(target, "w") as f:%c    f.write(s %% (10, 10, target, 10, s, 10, 10, 10, 10))%c'
+s = '#!/usr/bin/env python3%c%ctarget = %r%cs = %r%c# 🅱️%cwith open(target, "w") as f:%c    if not f.closed:%c        f.write(s %% (10, 10, target, 10, s, 10, 10, 10, 10, 10))%c'
 # 🅱️
 with open(target, "w") as f:
-    f.write(s % (10, 10, target, 10, s, 10, 10, 10, 10))
+    if not f.closed:
+        f.write(s % (10, 10, target, 10, s, 10, 10, 10, 10, 10))
