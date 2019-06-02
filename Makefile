617aa778cc0549d880e5ac54f040eb465faf7ea9
Alexander <alexandercarlson99@gmail.com>
2019-06-02 14:06:52 -0700

    Finished golang/Colleen.  Changed line endings from DOS to Unix

--- Makefile
+++ Makefile
@@ -1,13 +1,16 @@
 all:
 	make -C c/
 	make -C asm/
+	make -C golang/
 
 clean:
 	make -C c/ clean
 	make -C asm/ clean
+	make -C golang/ clean
 
 fclean: clean
 	make -C c/ fclean
 	make -C asm/ fclean
+	make -C golang/ fclean
 
 re: fclean all
--- golang/Colleen.go
+++ golang/Colleen.go
@@ -2,42 +2,42 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"net"
+	"os"
 )
 
-var msgs []string
-var data []int
+var s string
 
-func boi(intchan chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for {
-		i := <- intchan
-		if i >= len(msgs) {
-			return
-		}
-		fmt.Printf(msgs[i], data[i])
-	}
-}
-
-func fill(intchan chan int) {
-	for i := 0; i < 100; i++ {
-		intchan <- i
-	}
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	fmt.Fprintf(conn, s, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)
 }
 
 func main() {
-	msgs = []string{"%d\n", "%d\n", "%d\n"}
-	data = []int{1,69,100}
-	fmt.Printf("%d\n", len(msgs))
-	intchan := make(chan int)
-	var wg sync.WaitGroup
-	s := "A%dA\n"
-	fmt.Printf(s, 4)
-	go fill(intchan)
-	for i := 0; i < 4; i++ {
-		go boi(intchan, &wg)
-		wg.Add(1)
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Printf("Error listening on localhost:8080%c", 10)
+		os.Exit(1)
 	}
-	wg.Wait()
-	fmt.Printf(s, 4)
+	defer ln.Close()
+
+	/*
+	  Comment hehe
+	*/
+
+	s = "package main%c%cimport (%c	%cfmt%c%c	%cnet%c%c	%cos%c%c)%c%cvar s string%c%cfunc handleConn(conn net.Conn) {%c	defer conn.Close()%c	fmt.Fprintf(conn, s, 10, 10, 10, 34, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10)%c}%c%cfunc main() {%c	ln, err := net.Listen(%ctcp%c, %c:8080%c)%c	if err != nil {%c		fmt.Printf(%cError listening on localhost:8080%%c%c, 10)%c		os.Exit(1)%c	}%c	defer ln.Close()%c%c	/*%c	  Comment hehe%c	*/%c%c	s = %c%s%c%c%c	for {%c		conn, err := ln.Accept()%c		if err != nil {%c			fmt.Printf(%cError accepting connection%c)%c			continue%c		}%c		defer conn.Close()%c		go handleConn(conn)%c	}%c}%c%c/*%c  'nc localhost 8080' to read tha boi%c*/%c"
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting connection")
+			continue
+		}
+		defer conn.Close()
+		go handleConn(conn)
+	}
 }
+
+/*
+  'nc localhost 8080' to read tha boi
+*/
