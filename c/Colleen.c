e149fbf9e52548668eeffd93719f2a350c6e865a
Alexander <alexandercarlson99@gmail.com>
2019-06-01 17:00:35 -0700

    Fixed Colleen.  Added comment in main

--- c/Colleen.c
+++ c/Colleen.c
@@ -4,14 +4,13 @@
   One comment
 */
 
-/*
-  Two comment
-*/
-
 char	*other(void) {
-	return ("#include <stdio.h>%c%c/*%c  One comment%c*/%c%c/*%c  Two comment%c*/%c%cchar	*other(void) {%c%creturn (%c%s%c);%c}%c%cint		main(void) {%c%cprintf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);%c}%c");
+	return ("#include <stdio.h>%c%c/*%c  One comment%c*/%c%cchar	*other(void) {%c%creturn (%c%s%c);%c}%c%cint		main(void) {%c/*%c  Two comment%c*/%c	printf(other(), 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c");
 }
 
 int		main(void) {
-	printf(other(), 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 9, 10, 10, 10, 10);
+/*
+  Two comment
+*/
+	printf(other(), 10, 10, 10, 10, 10, 10, 10, 9, 34, other(), 34, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 }
--- python/Colleen.py
+++ python/Colleen.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 # Comment A
 def fun():
-    return '#! /usr/bin/env python3%c# Comment A%cdef fun():%c    return %r%c# Comment B%cprint(fun() %% (10, 10, 10, fun(), 10, 10))'
-# Comment B
-print(fun() % (10, 10, 10, fun(), 10, 10))
+    return '#! /usr/bin/env python3%c# Comment A%cdef fun():%c    return %r%cdef main():%c    # Comment B%c    print(fun() %% (10, 10, 10, fun(), 10, 10, 10, 10, 10))%c%cmain()'
+def main():
+    # Comment B
+    print(fun() % (10, 10, 10, fun(), 10, 10, 10, 10, 10))
+
+main()
