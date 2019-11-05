bcb4c271550a8b2006cafeeee69fad7169486f48
Alexander Carlson <bombblob@gmail.com>
2019-11-04 16:37:04 -0800

    Added newline to un-break Sully.go

--- golang/Sully.go
+++ golang/Sully.go
@@ -15,6 +15,6 @@ func main() {
 		return
 	}
 	target := fmt.Sprintf(basename, i-1)
-	s := "package main%cimport (%c\t%cfmt%c%c\t%cio/ioutil%c%c)%c%cconst (%c\ti = %d%c\tbasename = %q%c)%c%cfunc main() {%c\tif i <= 0 {%c\t\treturn%c\t}%c\ttarget := fmt.Sprintf(basename, i-1)%c\ts := %q%c\tioutil.WriteFile(target, []byte(fmt.Sprintf(s, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, i-1, 10, basename, 10, 10, 10, 10, 10, 10, 10, 10, s, 10, 10, 10)), 0644)%c}%c"
-	ioutil.WriteFile(target, []byte(fmt.Sprintf(s, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, i-1, 10, basename, 10, 10, 10, 10, 10, 10, 10, 10, s, 10, 10, 10)), 0644)
+	s := "package main%c%cimport (%c\t%cfmt%c%c\t%cio/ioutil%c%c)%c%cconst (%c\ti = %d%c\tbasename = %q%c)%c%cfunc main() {%c\tif i <= 0 {%c\t\treturn%c\t}%c\ttarget := fmt.Sprintf(basename, i-1)%c\ts := %q%c\tioutil.WriteFile(target, []byte(fmt.Sprintf(s, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, i-1, 10, basename, 10, 10, 10, 10, 10, 10, 10, 10, s, 10, 10, 10)), 0644)%c}%c"
+	ioutil.WriteFile(target, []byte(fmt.Sprintf(s, 10, 10, 10, 34, 34, 10, 34, 34, 10, 10, 10, 10, i-1, 10, basename, 10, 10, 10, 10, 10, 10, 10, 10, s, 10, 10, 10)), 0644)
 }
